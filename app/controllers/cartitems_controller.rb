bfc2e3aae04bf6b325c1ef8544aa4f1d0e4df8c3
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2016-08-29 12:43:24 +0100

    Refactor products table entries into partial

--- app/controllers/cartitems_controller.rb
+++ app/controllers/cartitems_controller.rb
@@ -1,10 +1,10 @@
 class CartitemsController < ApplicationController
 
   def create
-    @product = Product.find(params[:id])
-    @cartitem = Cartitem.new
+    @product          = Product.find(params[:id])
+    @cartitem         = Cartitem.new
     @cartitem.product = @product
-    @cartitem.cart = Cart.find(session[:cart_id])
+    @cartitem.cart    = Cart.find(session[:cart_id])
 
     if @cartitem.save
       respond_to :js
