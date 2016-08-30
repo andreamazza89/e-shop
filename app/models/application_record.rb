4913c05369dacafe6a95787c4052e0a0aedd3fb0
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2016-08-30 19:42:25 +0100

    Extract conversion from pence to ApplicationRecord

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,9 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def pence_to_pounds(total)
+    return '0.00' if total == 0
+    total.to_s[0..-3] + '.' + total.to_s[-2..-1]
+  end
+
 end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -10,11 +10,4 @@ class Cart < ApplicationRecord
     self.cartitems.reduce(0) { |sum, cartitem| sum += cartitem.product.price } - discount
   end
 
-  private
-  
-  def pence_to_pounds(total)
-    return '0.00' if total == 0
-    total.to_s[0..-3] + '.' + total.to_s[-2..-1]
-  end
-
 end
