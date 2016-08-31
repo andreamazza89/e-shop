742b94fe3bd67da22115e172df69d16b74412c85
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2016-08-31 09:28:12 +0100

    Memoize cart assignment if already registered to session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
   def connect_cart
     if session[:cart_id]
-      @cart = Cart.find(session[:cart_id])
+      @cart ||= Cart.find(session[:cart_id])
     else
       @cart = Cart.create
       session[:discount] = 0
