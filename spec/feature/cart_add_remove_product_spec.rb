a43ad7c0009d52cfbece62b805f71b85b9bc1f7c
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2016-09-29 12:13:54 +0100

    Prevents duplicates in cart

--- spec/feature/cart_add_remove_product_spec.rb
+++ spec/feature/cart_add_remove_product_spec.rb
@@ -12,6 +12,13 @@ feature 'Feature - Add/remove items to shopping cart' do
     end
   end
 
+  scenario 'Story 1.5: products show as single item in cart',focus: true, js: true do
+    click_on 'add to cart'
+    within '#cart' do
+      expect(page).to have_css('li', count: 1)
+    end
+  end
+
   scenario 'Story 2: As a User I can remove a product from my shopping cart.', js: true do
     within '#cart' do
       click_on 'remove from cart'
